f4daf55b2ba975be72fc48f2a068d8d4e0738afc
juanf-puerta <jfpuerta@icloud.com>
2024-05-16 11:49:17 -0500

    First Commit

---
+++ Python_git_test_1.py
@@ -0,0 +1,13 @@
+# Creating a new Python File for Git
+import math as m
+import pandas as pd
+
+# This is the cretion of variables to use
+a = 10
+b = 20
+c = 32
+
+# calculate square roots
+
+magnitude = m.sqrt(a^2+b^2+c^2)
+print(magnitude)
